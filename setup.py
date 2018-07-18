a69c94733d48fe3646507e405f6664a08acec0fa
xieqihuiPG <qihui@propertyguru.com.sg>
2018-07-18 18:25:24 +0800

    update version to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='pandas_multiprocess',
-    version='0.0.1',
+    version='0.0.2',
     author='Qihui Xie',
     author_email='xieqihui@gmail.com',
     maintainer='Qihui Xie',
