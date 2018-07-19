ea0a3bd767bfa2f3d8c081dfee88e9f6d3bc70be
xieqihuiPG <qihui@propertyguru.com.sg>
2018-07-19 13:19:15 +0800

    add class docstring

--- pandas_multiprocess/multiprocess.py
+++ pandas_multiprocess/multiprocess.py
@@ -1,21 +1,25 @@
-'''
-Function to use multiprocessing to process a pandas DataFrame.
-Usage:
-    from multiprocess import multi_process
-    def func(df, arg1, arg2):
-        " Function to manipulate the DataFrame df and return a dictionary
-        Returns: A list of dicts containing the results.
-        "
-        result_list_of_dict = apply_some_operations(df)
-        return result_list_of_dict
-    df_to_eval = pd.DataFrame(...)
-    args = {'arg1': arg1, 'arg2': arg2}
-    df_eval_result = multi_process(func=func,
-                                   data=df_to_eval,
-                                   chunk_size=100,
-                                   num_consumers=10,
-                                   **args)
-'''
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2018 Qihui Xie
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import multiprocessing
 import time
 import os
@@ -26,51 +30,82 @@ logger = logging.getLogger(__name__)
 
 
 class Consumer(multiprocessing.Process):
-    '''Class to generate multi processing
+    ''' Objects with this type work as processers in the multiprocessing job.
     '''
     def __init__(self, func, task_queue, result_queue, error_queue,
                  **args):
+        '''Constructs a `Consumer` instance
+
+        Args:
+            func (function): the function to apply on each row of the input
+                Dataframe
+            task_queue (multiprocessing.JoinableQueue): A queue of the
+                input data.
+            result_queue (multiprocessing.Queue): A queue to collect
+                the output of func.
+            error_queue (multiprocessing.Queue): A queue to collect
+                exception information of child processes.
+            args (dict): A dictionary of arguments to be passed to func.
+        '''
         multiprocessing.Process.__init__(self)
-        self.func = func
-        self.task_queue = task_queue
-        self.result_queue = result_queue
-        self.error_queue = error_queue
-        self.args = args
+        self._func = func
+        self._task_queue = task_queue
+        self._result_queue = result_queue
+        self._error_queue = error_queue
+        self._args = args
 
     def run(self):
+        '''Define the job of each process to run.
+        '''
         while True:
-            next_task = self.task_queue.get()
+            next_task = self._task_queue.get()
             # If there is any error, only consume data but not run the job
-            if self.error_queue.qsize() > 0:
-                self.task_queue.task_done()
+            if self._error_queue.qsize() > 0:
+                self._task_queue.task_done()
                 continue
             if next_task is None:
                 # Poison pill means shutdown
-                self.task_queue.task_done()
+                self._task_queue.task_done()
                 break
             try:
-                answer = self.func(next_task, **self.args)
-                self.task_queue.task_done()
-                self.result_queue.put(answer)
+                answer = self._func(next_task, **self._args)
+                self._task_queue.task_done()
+                self._result_queue.put(answer)
             except Exception as e:
-                self.task_queue.task_done()
-                self.error_queue.put((os.getpid(), e))
+                self._task_queue.task_done()
+                self._error_queue.put((os.getpid(), e))
                 logger.error(e)
                 continue
 
 
 class TaskTracker(multiprocessing.Process):
-    '''Class to generate multi processing
+    '''An object to track the progress of the multiprocessing job.
+
+    An object of this type will keep checking the amount of data remains in the
+    task queue and output the percentage of finished task.
+
+    Attributes:
+        total_task (int): Total number of tasks in the task queue at the
+            begining.
+        current_state (int): Current finished percentage of total tasks.
     '''
     def __init__(self, task_queue):
+        '''Construct an instance of TaskTracker
+
+        Args:
+            task_queue (multiprocessing.JoinableQueue): A queue of the
+                input data.
+        '''
         multiprocessing.Process.__init__(self)
-        self.task_queue = task_queue
-        self.total_task = self.task_queue.qsize()
+        self._task_queue = task_queue
+        self.total_task = self._task_queue.qsize()
         self.current_state = None
 
     def run(self):
+        '''Define the job of each process to run.
+        '''
         while True:
-            task_remain = self.task_queue.qsize()
+            task_remain = self._task_queue.qsize()
             task_finished = int((float(self.total_task - task_remain) /
                                  float(self.total_task)) * 100)
             if task_finished % 20 == 0 and task_finished != self.current_state:
@@ -81,42 +116,61 @@ class TaskTracker(multiprocessing.Process):
         logger.debug('All task data cleared')
 
 
-def multi_process(func, data, chunk_size, num_consumers, **args):
-    '''Function to use multiprocessing to analyze data
+def multi_process(func, data, chunk_size=None, num_process=None, **args):
+    '''Function to use multiprocessing to process pandas Dataframe.
+
+    This function applies a function on each row of the input DataFrame by
+    multiprocessing.
+
+    Args:
+        func (function): the function to apply on each row of the input
+            Dataframe
+        data (pandas.DataFrame): A DataFrame to be processed.
+        chunk_size (int, optional): how many rows of data to be sent in each
+            single process at a time. Defaults to be int(len(data)/num_process)
+        num_process (int, optional): the number of processes to run in
+            parallel. Defaults to be the number of CPUs of the computer.
     return:
         A dataframe containing the results
     '''
-    start_time = time.time()
+    # Check arguments value
+    assert isinstance(data, pd.DataFrame), \
+        'Input data must be a pandas.DataFrame instance'
+    if num_process is None:
+        num_process = multiprocessing.cpu_count()
+    if chunk_size is None:
+        chunk_size = int(np.ceil(len(data)/float(num_process)))
     # Establish communication queues
     tasks = multiprocessing.JoinableQueue()
     results = multiprocessing.Queue()
     error_queue = multiprocessing.Queue()
-
+    start_time = time.time()
     # Enqueue jobs
     num_jobs = int(np.ceil(len(data)/float(chunk_size)))
     for i in range(num_jobs):
         tasks.put(data[i*chunk_size:min((i+1)*chunk_size, len(data))])
     # Add a poison pill for each consumer
-    for i in range(num_consumers):
+    for i in range(num_process):
         tasks.put(None)
 
-    # num_consumers = int(multiprocessing.cpu_count()*0.98)
-    logger.info('Creating %d consumers' % num_consumers)
+    logger.info('Create {} processes'.format(num_process))
     consumers = [Consumer(func, tasks, results, error_queue, **args)
-                 for i in range(num_consumers)]
+                 for i in range(num_process)]
     for w in consumers:
         w.start()
+    # Add a task tracking process
     task_tracker = TaskTracker(tasks)
     task_tracker.start()
-    # Wait for all input data to be comsumed
+    # Wait for all input data to be processed
     tasks.join()
+    # If there is any error in any process, output the error messages
     num_error = error_queue.qsize()
     if num_error > 0:
         for i in range(num_error):
             logger.error(error_queue.get())
         raise RuntimeError('Multi process jobs failed')
     else:
-        # Start printing results
+        # Collect results
         result_table = []
         while num_jobs:
             result_table += results.get()
